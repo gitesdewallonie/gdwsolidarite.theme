341c32c1614ed1a8607d9cb669a515f818357a4f
Laurent Lasudry <lasudry@gmail.com>
2015-06-26 16:29:50 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+gdwsolidarite.theme
+===================
+
+Theme package for Action Solidarite one page site
+
+
+Credits
+-------
+
+This package was developed by `Affinitic team <https://github/affinitic>`_.
+
+.. image:: http://www.affinitic.be/affinitic_logo.png
+   :alt: Affinitic website
+   :target: http://www.affinitic.be
+
+``gdwsolidarite.theme`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='gdwsolidarite.theme',
+      version=version,
+      description="Theme package for Action Solidarite one page site",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Affinitic',
+      author='Affinitic',
+      author_email='info@affinitic.be',
+      url='https://github.com/gitesdewallonie/gdwsolidarite.theme',
+      license='gpl',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['gdwsolidarite'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.themingplugins',
+          # -*- Extra requirements: -*-
+      ])
