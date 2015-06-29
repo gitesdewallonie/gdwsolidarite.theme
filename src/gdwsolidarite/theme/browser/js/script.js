f824ff3c9d27481755542240200c7c7fbff18fe0
Alain Meurant <alain.meurant@affinitic.be>
2015-06-29 19:48:34 +0200

    Une skin, une !
    les images
    les css
    les js

---
+++ src/gdwsolidarite/theme/browser/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#accueil').parallax("center",2, true);
+    $('#action').parallax("center",0.5, true);
+    $('#partenaire1').parallax("center",0.5, true);
+    $('#partenaire2').parallax("center",0.5, true);
+    $('#hebergement').parallax("center",0.5, true);
+    $('#federation').parallax("center",0.5, true);
+    $('#reseaux-sociaux').parallax("center",0.5, true);
+    $('#contact').parallax("center",0.5, true);
+})
