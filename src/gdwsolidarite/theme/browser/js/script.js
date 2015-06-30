5552cbacc61a166b8dbeaa460fc03bcca1073b10
Alain Meurant <alain.meurant@affinitic.be>
2015-06-30 17:09:11 +0200

    GDW Social action
    Mise en place des différentes section avec Kévine
    Mise en place des différents background seon les sections
    Mise en place des séparations de section par des blocs images
    Ré activation des JS
    Ré activtion de la nav responsive

--- src/gdwsolidarite/theme/browser/js/script.js
+++ src/gdwsolidarite/theme/browser/js/script.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-    alert('coucou');
-    $('#accueil').parallax("center",2, true);
+    $('#accueil').parallax("center",0, true);
     $('#action').parallax("center",0.5, true);
-    $('#partenaire1').parallax("center",0.5, true);
-    $('#partenaire2').parallax("center",0.5, true);
+    $('#partenaire').parallax("center",0.5, true);
     $('#hebergement').parallax("center",0.5, true);
-    $('#federation').parallax("center",0.5, true);
+    $('#a-propos').parallax("center",0.5, true);
     $('#reseaux-sociaux').parallax("center",0.5, true);
-    $('#contact').parallax("center",0.5, true);
 })
+
