0c864d0e5a2d53611223c567db4262ae41c3c0da
Alain Meurant <alain.meurant@affinitic.be>
2015-07-31 18:31:56 +0200

    Ajouter la section attractions et musées

--- src/gdwsolidarite/theme/browser/js/script.js
+++ src/gdwsolidarite/theme/browser/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('#accueil').parallax("center",0, true);
     $('#action').parallax("center",0.5, true);
-    $('#partenaire').parallax("center",0.5, true);
     $('#hebergement').parallax("center",0.5, true);
+    $('#attractions-musees').parallax("center",0.5, true);
+    $('#partenaire').parallax("center",0.5, true);
     $('#a-propos').parallax("center",0.5, true);
     $('#reseaux-sociaux').parallax("center",0.5, true);
 })
